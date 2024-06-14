f4d43f9d06ff0e8390524be58f4cecf8d4517dc2
karen <melanie171095@gmail.com>
2024-06-14 13:19:11 -0500

    agrega cambios aview.js

--- src/view.js
+++ src/view.js
@@ -1,6 +1,23 @@
 export const renderItems = (data) => {
-  console.log(data)
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+  
+  //Crear un elemento <ul> utilizando el método createElement.
+  const ul = document.createElement("ul");
+  //Recorrer la data mediante un bucle o algún método de array como forEach o map
+  data.forEach(item => {
+    //Por cada elemento de la data, crear un elemento <li>.
+    const li = document.createElement("li");
+    li.textContent = item;
+    //Agregar cada <li> al <ul> utilizando innerHTML , appendChild u otro método apropiado.
+    ul.appendChild(li);  
+    
+  });
+  //Finalmente, retornar el elemento <ul>
+   return ul;
+
+
+
+ // console.log(data)
+  
+// return 'example';
 };
 
