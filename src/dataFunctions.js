eccf71ba127b195591a011172d2558e631d7e552
CamilaCarrenoV <camila.carreno89@gmail.com>
2024-06-28 19:14:12 -0400

    Empezando hito3

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,1 +1,19 @@
+const dataFunctions = {
+
+filterByEpocaUno = (data) => {
+    return data.filter(data => data.facts.estreno >= 1990 && data.facts.estreno <=1999);
+},
+
+filterByEpocaDos = (data) => {
+    return data.filter(data => data.facts.estreno >= 2000 && data.facts.estreno <=2009);
+},
+    
+filterByEpocaTres = (data) => {
+    return data.filter(data => data.facts.estreno >= 2010 && data.facts.estreno <=2019);
+   },
+
+filterByEpocaCuatro = (data) => {
+    return data.filter(data => data.facts.estreno >= 2020 && data.facts.estreno <=2029);
+   },
+},
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,44 @@ import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
+
+
 const elementos = document.getElementById("disneyinfo");
 const listaDePeliculas = renderItems(data);
 elementos.appendChild(listaDePeliculas);
+
+
+const selector90 = document.querySelector('option[value="1990"]');
+const selector2000 = document.querySelector('option[value="2000"]');
+const selector2010 = document.querySelector('option[value="2010"]');
+const selector2020 = document.querySelector('option[value="2020"]');
+
+//Crear función de filtrado
+//aplicar la función de filtrado de los 90 al selector de los 90
+//decirle que al hacer click al selector de los 90 se aplique el filtrado
+
+function filtrarAño90() {
+const filtrarAño90 = dataFunctions.filterByEpocaUno();
+selector90.innerHTML = filtrarAño90;
+}
+selector90.addEventListener("click", filtrarAño90);
+
+function filtrarAño2000() {
+const filtrarAño00 = dataFunctions.filterByEpocaDos();
+selector2000.innerHTML = filtrarAño00;
+}
+selector2000.addEventListener("click", filtrarAño2000);
+
+function filtrarAño2010() {
+const filtrarAño10 = dataFunctions.filterByEpocaTres();
+selector2010.innerHTML = filtrarAño10;
+}
+selector2010.addEventListener("click", filtrarAño2010);
+
+function filtrarAño2020(){
+const filtrarAño20 = dataFunctions.filterByEpocaCuatro();
+selector2020.innerHTML = filtrarAño20;
+}
+selector2020.addEventListener("click", filtrarAño2020);
+
+
